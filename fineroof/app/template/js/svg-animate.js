bc5fc3c07dc029a7fbed8e78e8cdab67eb766451
dial067 <dial067@dialweb.local>
2019-02-01 18:01:01 +0300

    Правки

--- fineroof/app/template/js/svg-animate.js
+++ fineroof/app/template/js/svg-animate.js
@@ -23,195 +23,209 @@
 		var mapButton = document.querySelector('.header__button');
 		var mapLane = document.getElementById("mainPin");
 
-		mapLane.addEventListener('load', function() {
-			mapButton.onmouseover = mapButton.onmouseout = handler;
-			var mapDocument = mapLane.contentDocument;
-			var mapPath = mapDocument.querySelector('.hover-animation_background');
+		if (mapLane !== null) {
+			mapLane.addEventListener('load', function() {
+				mapButton.onmouseover = mapButton.onmouseout = handler;
+				var mapDocument = mapLane.contentDocument;
+				var mapPath = mapDocument.querySelector('.hover-animation_background');
 
-			function handler (event) {
-				if (event.type == 'mouseover') {
-					mapPath.style.x = "-35";
-					mapPath.style.y = "40";
-					window.script.removePseudo();
-				}
-				if (event.type == 'mouseout') {
-					mapPath.style.x = "-5";
-					mapPath.style.y = "-5";
-					window.script.removePseudo();
-				}
-			};
-		});
+				function handler (event) {
+					if (event.type == 'mouseover') {
+						mapPath.style.x = "-35";
+						mapPath.style.y = "40";
+						window.script.removePseudo();
+					}
+					if (event.type == 'mouseout') {
+						mapPath.style.x = "-5";
+						mapPath.style.y = "-5";
+						window.script.removePseudo();
+					}
+				};
+			});
+		}
 
 	}
 	var createNSupportDraw = function(currentSvg, animatedElement, dur) {
-		currentSvg.addEventListener('load', function () {
-			var svgDocument = currentSvg.contentDocument;
-			var animateSvg = function () {
+		if (currentSvg !== null) {
+			currentSvg.addEventListener('load', function () {
+				var svgDocument = currentSvg.contentDocument;
+				var animateSvg = function () {
 
-				var animationTarget = svgDocument.querySelector(animatedElement);
-				var lineDrawing = anime({
-					targets: animationTarget,
-					strokeDashoffset: [anime.setDashoffset, 0],
-					easing: 'linear',
-					delay: 400,
-					duration: dur
-				});
+					var animationTarget = svgDocument.querySelector(animatedElement);
+					var lineDrawing = anime({
+						targets: animationTarget,
+						strokeDashoffset: [anime.setDashoffset, 0],
+						easing: 'linear',
+						delay: 400,
+						duration: dur
+					});
 
-				window.addEventListener('scroll', function () {
-					let wHeight = this.innerHeight;
-					document.querySelectorAll('.jsAnimate').forEach(function (item) {
+					window.addEventListener('scroll', function () {
+						let wHeight = this.innerHeight;
+						document.querySelectorAll('.jsAnimate').forEach(function (item) {
 
-						function getCoords() {
-							return {
-								itemTop: item.getBoundingClientRect().top + pageYOffset,
-							};
-						}
-						let windowScroll = window.pageYOffset;
+							function getCoords() {
+								return {
+									itemTop: item.getBoundingClientRect().top + pageYOffset,
+								};
+							}
+							let windowScroll = window.pageYOffset;
 
-						if ((windowScroll + wHeight > getCoords().itemTop) && window.utils.visibility(item) === false && ((windowScroll + wHeight - getCoords().itemTop) <= 200)) {
-							lineDrawing.restart();
-						} else {
-							return;
-						}
-					})
-				});
+							if ((windowScroll + wHeight > getCoords().itemTop) && window.utils.visibility(item) === false && ((windowScroll + wHeight - getCoords().itemTop) <= 200)) {
+								lineDrawing.restart();
+							} else {
+								return;
+							}
+						})
+					});
 
-			}
-			animateSvg();
-		});
+				}
+				animateSvg();
+			});
+		}
 	}
 
 	var hoverDrowEffect = function (parent) {
 		var childObj = parent.querySelector('.hoverDraw');
-		childObj.addEventListener('load', function () {
-			var getChildCont = childObj.contentDocument;
-			var animationTarget = getChildCont.querySelector('.hoverDrawTarget');
+		if (childObj !== null) {
+			childObj.addEventListener('load', function () {
+				var getChildCont = childObj.contentDocument;
+				var animationTarget = getChildCont.querySelector('.hoverDrawTarget');
 
-			parent.onmouseover = parent.onmouseout = handler;
+				parent.onmouseover = parent.onmouseout = handler;
 
-			var status = false;
-			var animation = anime({
-				targets: animationTarget,
-				strokeDashoffset: [anime.setDashoffset, 0],
-				easing: 'linear',
-				delay: 0,
-				duration: 10000,
-				autoplay: false
+				var status = false;
+				var animation = anime({
+					targets: animationTarget,
+					strokeDashoffset: [anime.setDashoffset, 0],
+					easing: 'linear',
+					delay: 0,
+					duration: 10000,
+					autoplay: false
+				});
+				function handler(event) {
+					if (event.type == 'mouseover' && status === false) {
+						status = true;
+						animation.restart();
+					}
+					if (event.type == 'mouseout' && status === true) {
+						animation.reverse();
+						animation.finished.then(status = false);
+					}
+				};
+
 			});
-			function handler(event) {
-				if (event.type == 'mouseover' && status === false) {
-					status = true;
-					animation.restart();
-				}
-				if (event.type == 'mouseout' && status === true) {
-					animation.reverse();
-					animation.finished.then(status = false);
-				}
-			};
-
-		});
+		}
 
 	}
 	var animatedGalery = function() {
 		var svgWrapper = document.querySelector('.gallery__wrap');
-		var SVG = document.querySelector('.galery-house');
-		SVG.addEventListener('load', function(){
-			svgWrapper.onmouseover = svgWrapper.onmouseout = handler;
-			var animationUp = SVG.contentDocument.querySelector('.lights-up');
-			var recolorElems = SVG.contentDocument.querySelectorAll('.recolor-elems');
-			function handler(event) {
+		if (svgWrapper !== null) {
+			var SVG = document.querySelector('.galery-house');
+		}
+		if (SVG !== null) {
+			SVG.addEventListener('load', function(){
+				svgWrapper.onmouseover = svgWrapper.onmouseout = handler;
+				var animationUp = SVG.contentDocument.querySelector('.lights-up');
+				var recolorElems = SVG.contentDocument.querySelectorAll('.recolor-elems');
+				function handler(event) {
 
-				if (event.type == 'mouseover') {
-					anime({
-						targets: animationUp,
-						translateY: -10
-					});
-					recolorElems.forEach(function (elem) {
+					if (event.type == 'mouseover') {
 						anime({
-						  targets: elem,
-						  fill: '#1bc09b',
-						  easing: 'easeInOutSine',
-						  duration: 500
+							targets: animationUp,
+							translateY: -10
 						});
-					})
-				}
-				if (event.type == 'mouseout') {
-					anime({
-						targets: animationUp,
-						translateY: 0
-					});
-					recolorElems.forEach(function (elem) {
+						recolorElems.forEach(function (elem) {
+							anime({
+								targets: elem,
+								fill: '#1bc09b',
+								easing: 'easeInOutSine',
+								duration: 500
+							});
+						})
+					}
+					if (event.type == 'mouseout') {
 						anime({
-						  targets: elem,
-						  fill: '#DAE9E5',
-						  easing: 'easeInOutSine',
-				   		  duration: 500
+							targets: animationUp,
+							translateY: 0
 						});
-					})
-				}
-			};
-		})
+						recolorElems.forEach(function (elem) {
+							anime({
+								targets: elem,
+								fill: '#DAE9E5',
+								easing: 'easeInOutSine',
+								duration: 500
+							});
+						})
+					}
+				};
+			})
+		}
 	}
 	var animatedLogo = function() {
 		var svgWrapper = document.querySelector('.header__image');
 		var SVG = svgWrapper.querySelector('object');
-		SVG.addEventListener('load', function(){
-			svgWrapper.onmouseover = svgWrapper.onmouseout = handler;
-			var animationUp = SVG.contentDocument.querySelector('.hover-animation_logo');
-			function handler(event) {
+		if (SVG !== null) {
+			SVG.addEventListener('load', function(){
+				svgWrapper.onmouseover = svgWrapper.onmouseout = handler;
+				var animationUp = SVG.contentDocument.querySelector('.hover-animation_logo');
+				function handler(event) {
 
-				if (event.type == 'mouseover') {
-					anime({
-						targets: animationUp,
-						translateX: 20,
-						easing: 'easeInOutBack',
-						duration: 600
+					if (event.type == 'mouseover') {
+						anime({
+							targets: animationUp,
+							translateX: 20,
+							easing: 'easeInOutBack',
+							duration: 600
 
-					});
-				}
-				if (event.type == 'mouseout') {
-					anime({
-						targets: animationUp,
-						translateX: 0,
-						easing: 'easeInOutBack',
-						duration: 600
-					});
-				}
-			};
-		})
+						});
+					}
+					if (event.type == 'mouseout') {
+						anime({
+							targets: animationUp,
+							translateX: 0,
+							easing: 'easeInOutBack',
+							duration: 600
+						});
+					}
+				};
+			})
+		}
 	}
 	var animatedYt = function() {
 		var svgWrapper = document.querySelector('.jsChannel');
 		var SVG = svgWrapper.querySelector('object');
-		SVG.addEventListener('load', function(){
-			svgWrapper.onmouseover = svgWrapper.onmouseout = handler;
-			var animationUp = SVG.contentDocument.querySelector('.hover-animation_play');
-			function handler(event) {
+		if (SVG !== null) {
+			SVG.addEventListener('load', function(){
+				svgWrapper.onmouseover = svgWrapper.onmouseout = handler;
+				var animationUp = SVG.contentDocument.querySelector('.hover-animation_play');
+				function handler(event) {
 
-				if (event.type == 'mouseover') {
-					anime({
-						targets: animationUp,
-						translateX: 11,
-						translateY: 7,
+					if (event.type == 'mouseover') {
+						anime({
+							targets: animationUp,
+							translateX: 11,
+							translateY: 7,
 
-						scale: 0.7,
-						easing: 'easeInOutBack',
-						duration: 600
+							scale: 0.7,
+							easing: 'easeInOutBack',
+							duration: 600
 
-					});
-				}
-				if (event.type == 'mouseout') {
-					anime({
-						targets: animationUp,
-						translateX: 0,
-						translateY: 0,
-						scale: 1,
-						easing: 'easeInOutBack',
-						duration: 600
-					});
-				}
-			};
-		})
+						});
+					}
+					if (event.type == 'mouseout') {
+						anime({
+							targets: animationUp,
+							translateX: 0,
+							translateY: 0,
+							scale: 1,
+							easing: 'easeInOutBack',
+							duration: 600
+						});
+					}
+				};
+			})
+		}
 	}
 
 	handleMapElement();
